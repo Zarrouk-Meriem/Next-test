6c9a6bf77f2f58e56bfe2a89086ac098d04977cb
Zarrouk-Meriem <zarroukmeriem19@gmail.com>
2025-05-13 15:44:36 +0100

    fix the star icon and the page title

--- app/_components/_Shared/Rating.tsx
+++ app/_components/_Shared/Rating.tsx
@@ -5,7 +5,7 @@ function Rating({ rating }: { rating: number }) {
 		<span className='flex gap-[4px] text-[#1D2026] items-center justify-between text-[14px] font-[500]'>
 			<Image
 				alt='star'
-				src={"/assets/icons/star.svg"}
+				src={"/assets/icons/Star.svg"}
 				height={16}
 				width={16}
 				className='mb-[2px]'
